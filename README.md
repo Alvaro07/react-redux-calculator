f423e02843cc233158c89bc3d21f96b62869ef16
Alvaro AVGS. Gonzalez Serrano <alvaro.gonzalez@stkproj.com>
2018-12-12 13:37:05 +0100

    Updated readme and deploy to github Pages

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# react-redux-calculator
-A React calculator with Redux
+# A React Calculator with Redux
+
+A calculator developed in React.js with Redux connection deployed in Github pages.
+
+Link: [alvaro07.github.io/react-redux-calculator](https://alvaro07.github.io/react-redux-calculator/)
+
+---
+
+![alt text](/preview-image.png "Preview image")
---
+++ build/precache-manifest.eea0ca729fabf78293273af14972ede0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2ecf2e68de309a6bd2b",
+    "url": "/react-calculator/static/js/runtime~main.a2ecf2e6.js"
+  },
+  {
+    "revision": "beb724922ffe0f6889c7",
+    "url": "/react-calculator/static/js/main.beb72492.chunk.js"
+  },
+  {
+    "revision": "ce9c3b8a2ae76942d42e",
+    "url": "/react-calculator/static/js/1.ce9c3b8a.chunk.js"
+  },
+  {
+    "revision": "beb724922ffe0f6889c7",
+    "url": "/react-calculator/static/css/main.7f6b7549.chunk.css"
+  },
+  {
+    "revision": "e4d4af394df82fd52794ff2e018b99c3",
+    "url": "/react-calculator/index.html"
+  }
+];
